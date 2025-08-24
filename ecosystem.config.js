9924073e162fc686725e8f8d42e7d2c71bbdbce3
leonid_shuliak <zeleny777@gmail.com>
2025-08-24 20:54:03 +0300

    deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 5100
       },
       error_file: './logs/err.log',
       out_file: './logs/out.log',
