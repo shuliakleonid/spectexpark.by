6ea8b142f33cacfafd69c079f3eb2e8337dbda6e
leonid_shuliak <zeleny777@gmail.com>
2025-08-24 20:29:14 +0300

    deploy

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/spectexpark.by' : '',
   images: {
     unoptimized: true
   }
