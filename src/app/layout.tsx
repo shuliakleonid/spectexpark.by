13039e1ddeb7e3a1a92f7c58c806f9b8bc2fe25c
leonid_shuliak <zeleny777@gmail.com>
2025-11-27 10:30:50 +0300

    add yandex metrix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             gtag('config', 'G-N6QFRCPZWM');
           `}
         </Script>
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105521399', 'ym');
+            ym(105521399, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/105521399" style={{position:'absolute', left:'-9999px'}} alt="" />
+          </div>
+        </noscript>
       </head>
       <body className={`${inter.className} antialiased`}>
         <div className="min-h-screen bg-background text-foreground transition-colors duration-300">
